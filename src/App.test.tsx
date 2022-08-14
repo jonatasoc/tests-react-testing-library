9fc7dc94af3e23a6c759393bfca02db9d784ab1e
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2022-08-13 22:13:21 -0300

    initial commit and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  expect(screen.getByText(/change to blue/i)).toBeVisible();
 });
